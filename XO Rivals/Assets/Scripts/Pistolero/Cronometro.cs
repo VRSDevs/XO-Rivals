f41b7eaaa8a8cf3a187f8f548579ba6680a23337
kalomano <sowerpablo@gmail.com>
2021-10-07 11:27:17 +0200

    Sistema de juego completo
    El funcionamiento base del minijuego estaría completo, le falta arte y pulir.

--- XO Rivals/Assets/Scripts/Pistolero/Cronometro.cs
+++ XO Rivals/Assets/Scripts/Pistolero/Cronometro.cs
@@ -2,14 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cronometro : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public float time = 5;
+    public float time = 8;
+    public int tiempoDesaparecido = 4;
     public bool activo = false;
-    public TextMesh text;
+    public Text textoCrono;
+    public Text textoExplicativo;
 
     void Start()
     {
@@ -32,9 +35,37 @@ public class Cronometro : MonoBehaviour
             int decimas = (int)((time - segundos)*100); //Sacamos dos decimas
             decimas = Math.Abs(decimas);//el signo negativo solo tiene que estar en los segundos
 
-            text.text = segundos+":"+decimas;
+            textoCrono.text = segundos+":"+decimas;
+
+            //LOS ULTIMOS TRES SEGUNDOS DESAPARECE EL TIEMPO
+            if (segundos<tiempoDesaparecido )
+            {
+                textoCrono.text = "-" + ":" + "--";
+            }
 
         }
 
     }
+
+    public void activarCrono()
+    {
+        activo = true;
+        textoExplicativo.text = "Suelta el botón cuando el cronometro llegue a 0:00";
+
+    }
+    public void finCrono()
+    {
+        activo = false;
+
+        //SEPARAMOS SEGUNDOS Y DECIMAS DEL TIEMPO RESULTADO
+        int segundos = (int)Math.Truncate(time);
+        int decimas = (int)((time - segundos) * 100); //Sacamos dos decimas
+        decimas = Math.Abs(decimas);//el signo negativo solo tiene que estar en los segundos
+
+        textoCrono.text = segundos + ":" + decimas;
+        textoExplicativo.text = "ESTE ES TU RESULTADO!";
+
+    }
+
+
 }
