31b6fe6995fa500ac39a617602ad85ca3319b1f1
Antonio Ruano <blinx2500@gmail.com>
2021-10-23 10:56:58 +0200

    Creación de scripts

---
+++ XO Rivals/Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,22 @@
+using System;
+using BrainCloud.LitJson;
+
+public class PlayerInfo
+{
+    //************************* VARIABLES *************************//
+    #region Vars
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Name;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string PlayerId;
+
+    #endregion
+    
+
+}
