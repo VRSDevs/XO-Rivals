7652d9e564afd37535a02f3361b9be0d9c202884
Nachete07 <ignaciocallesicq@gmail.com>
2021-10-23 12:05:28 +0200

    Colisiones

--- XO Rivals/Assets/Scripts/Plataformas/Movement.cs
+++ XO Rivals/Assets/Scripts/Plataformas/Movement.cs
@@ -18,7 +18,7 @@ public class Movement : MonoBehaviour
     [SerializeField] private float Speed = 3, jumpSpeed = 4;
     public bool Victoria = false;
     public Transform respawn;
-
+    private Vector2 respawnPlayer;
     public GameObject player;
 
     // Start is called before the first frame update
@@ -60,28 +60,34 @@ public class Movement : MonoBehaviour
 
         }
     }
-    
 
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (collision.gameObject.tag == "Ground")
-        {
-            grounded = true;
-        }
 
-        if (collision.gameObject.tag == "Victory")
+        if (other.gameObject.CompareTag("Victory"))
         {
             Debug.Log("Victoria");
             textValue = "Victory";
             Victoria = true;
         }
+    }
 
-        if (collision.gameObject.tag == "Muerte")
-        {
-            Debug.Log("Game Over");
-            textValue = "Game Over";
-            player.transform.position = respawn.transform.position;
-        }
+ 
+  private void OnCollisionEnter2D(Collision2D collision)
+    {
         
+        
+               if (collision.gameObject.tag == "Muerte")
+               {
+                   Debug.Log("Game Over");
+                   textValue = "Game Over";
+                   player.transform.position = ;
+               }
+
+               if (collision.gameObject.tag == "Ground") {
+                grounded = true; 
+                
+               }
     }
 }
