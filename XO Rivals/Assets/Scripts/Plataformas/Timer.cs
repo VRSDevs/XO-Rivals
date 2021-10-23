461738ce3585d6785964e87a400a859d4a5c0579
Nachete07 <ignaciocallesicq@gmail.com>
2021-10-23 14:12:29 +0200

    StopTimeVictory

--- XO Rivals/Assets/Scripts/Plataformas/Timer.cs
+++ XO Rivals/Assets/Scripts/Plataformas/Timer.cs
@@ -22,13 +22,12 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (time >= 0)
+        if (time >= 0 && ScriptPlayer.Victory == false)
         {
             time -= Time.deltaTime;
             crono.text = " " + time.ToString("f0"); 
         }
-
-
+        
         if (time < 0)
         {
             if (ScriptPlayer.Victory == false)
