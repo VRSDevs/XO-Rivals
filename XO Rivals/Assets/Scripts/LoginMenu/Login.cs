7fa12983f642c917cfff5fc720135be2685fc34f
Antonio Ruano <blinx2500@gmail.com>
2021-10-25 21:12:12 +0200

    Ajuste Login.cs e implementación del sistema de autentificación

--- XO Rivals/Assets/Scripts/LoginMenu/Login.cs
+++ XO Rivals/Assets/Scripts/LoginMenu/Login.cs
@@ -5,14 +5,32 @@ using TMPro;
 using UnityEditor;
 using UnityEngine;
 
+[Serializable]
+public enum LoginMode
+{
+    NONE,
+    REGISTER,
+    LOGIN
+}
+
 public class Login : MonoBehaviour
 {
     #region Variables
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    [NonSerialized] public LoginMode Mode;
 
     /// <summary>
     /// 
     /// </summary>
     [SerializeField] public GameManager _gameManager;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField] public PlayFabAuthenticator Authenticator;
     
     /// <summary>
     /// 
@@ -27,16 +45,30 @@ public class Login : MonoBehaviour
     /// Máximo número de caracteres para nombre de usuario y contraseña
     /// </summary>
     private const int MAX_CHARS = 24;
+    
     /// <summary>
     /// 
     /// </summary>
-    [SerializeField] public TMP_InputField UsernameInput;
+    [SerializeField] public TMP_InputField R_UsernameInput;
     /// <summary>
     /// 
     /// </summary>
-    [SerializeField] public TMP_InputField PasswordInput;
+    [SerializeField] public TMP_InputField R_PasswordInput;
     /// <summary>
     /// 
+    /// </summary>
+    [SerializeField] public TMP_Text RegisterInfo;
+
+    /// <summary>
+    /// Referencia (en Login) al InputField del nombre de usuario
+    /// </summary>
+    [SerializeField] public TMP_InputField L_UsernameInput;
+    /// <summary>
+    /// Referencia (en Login) al InputField de la constraseña
+    /// </summary>
+    [SerializeField] public TMP_InputField L_PasswordInput;
+    /// <summary>
+    /// Referencia al log de información del login
     /// </summary>
     [SerializeField] public TMP_Text LoginInfo;
 
@@ -46,59 +78,79 @@ public class Login : MonoBehaviour
 
     private void Start()
     {
-        UsernameInput.characterLimit = MAX_CHARS;
-        PasswordInput.characterLimit = MAX_CHARS;
+        L_UsernameInput.characterLimit = MAX_CHARS;
+        L_PasswordInput.characterLimit = MAX_CHARS;
+        R_UsernameInput.characterLimit = MAX_CHARS;
+        R_PasswordInput.characterLimit = MAX_CHARS;
 
+        RegisterInfo.text = "";
         LoginInfo.text = "";
-        
-        //OnReconnect();
     }
 
     #endregion
 
     #region LoginMethods
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="mode"></param>
     public void OnConnect()
     {
-        if (!IsConnecting && ValidateLoginInputs())
+        if (!IsConnecting && ValidateInputs())
         {
             IsConnecting = true;
-            LoginInfo.text = "Conectando...";
 
-            if (_gameManager._networkController.OnConnectToServer())
+            switch (Mode)
             {
-                LoginInfo.text = "Conectado.";
-                _gameManager.Username = UsernameInput.text;
-                _gameManager._networkController.SetNickName(UsernameInput.text);
-            }
-            else
-            {
-                LoginInfo.text = "Error al conectar.";
+                case LoginMode.REGISTER:
+                    
+                    RegisterInfo.text = "Conectando...";
+                    
+                    if (OnAuthentication(R_UsernameInput.text, R_PasswordInput.text))
+                    {
+                        if (_gameManager._networkController.OnConnectToServer())
+                        {
+                            RegisterInfo.text = "Conectado.";
+                            _gameManager.Username = R_UsernameInput.text;
+                            _gameManager._networkController.SetNickName(R_UsernameInput.text);
+                        }
+                        else
+                        {
+                            LoginInfo.text = "Error al conectar.";
+                            IsConnecting = false;
+                        }
+                    }
+                    
+                    break;
+                case LoginMode.LOGIN:
+                    
+                    LoginInfo.text = "Conectando...";
+                    
+                    if (OnAuthentication(L_UsernameInput.text, L_PasswordInput.text))
+                    {
+                        if (_gameManager._networkController.OnConnectToServer())
+                        {
+                            LoginInfo.text = "Conectado.";
+                            _gameManager.Username = L_UsernameInput.text;
+                            _gameManager._networkController.SetNickName(L_UsernameInput.text);
+                        }
+                        else
+                        {
+                            LoginInfo.text = "Error al conectar.";
+                            IsConnecting = false;
+                        }
+                    }
+                    
+                    break;
             }
         }
     }
 
-    private void OnReconnect()
+    private bool OnAuthentication(string username, string password)
     {
-  
-    }
-
-    private void OnAuthentication(string response, object cbObject)
-    {
-        /*
-        var data = JsonMapper.ToObject(response)["data"];
-        _gameManager.UserId = data["profileId"].ToString();
-        _gameManager.Username = data["playerName"].ToString();
-        
-        PlayerPrefs.SetString(_gameManager.Username + "_hasAuthenticated", "true");
-        
-        bool isNewUser = data["newUser"].ToString().Equals("true");
-
-        if (isNewUser)
-        {
-            SetupPlayer();
-        }
-        */
+        Authenticator.AuthWithPlayfab(username, password, Mode);
+        return false;
     }
 
     #endregion
@@ -109,20 +161,39 @@ public class Login : MonoBehaviour
     /// 
     /// </summary>
     /// <returns></returns>
-    private bool ValidateLoginInputs()
+    private bool ValidateInputs()
     {
-        //
-        UsernameInput.text = UsernameInput.text.Trim();
-        PasswordInput.text = PasswordInput.text.Trim();
-        
-        //
-        if (UsernameInput.text.Length < MIN_CHARS /*|| PasswordInput.text.Length < MIN_CHARS*/)
+        switch (Mode)
         {
-            LoginInfo.text = "Longitud no correcta, mínimo " + MIN_CHARS;
-            return false;
+            case LoginMode.REGISTER:
+                //
+                R_UsernameInput.text = R_UsernameInput.text.Trim();
+                R_PasswordInput.text = R_PasswordInput.text.Trim();
+        
+                //
+                if (R_UsernameInput.text.Length < MIN_CHARS || R_UsernameInput.text.Length < MIN_CHARS)
+                {
+                    RegisterInfo.text = "Longitud no correcta, mínimo " + MIN_CHARS;
+                    return false;
+                }
+
+                return true;
+            case LoginMode.LOGIN:
+                //
+                L_UsernameInput.text = L_UsernameInput.text.Trim();
+                L_PasswordInput.text = L_PasswordInput.text.Trim();
+        
+                //
+                if (L_UsernameInput.text.Length < MIN_CHARS || L_UsernameInput.text.Length < MIN_CHARS)
+                {
+                    LoginInfo.text = "Longitud no correcta, mínimo " + MIN_CHARS;
+                    return false;
+                }
+
+                return true;
         }
 
-        return true;
+        return false;
     }
     
 
@@ -130,15 +201,20 @@ public class Login : MonoBehaviour
 
     #region OtherMethods
 
-    private void SetupPlayer()
+    public void UpdateLoginMode(int mode)
     {
-        /*
-        _gameManager.Server.PlayerStateService.UpdateName(UsernameInput.text,
-            (jsonResponse, o) =>
-            {
-                Debug.Log("Inició sesión");
-            });
-            */
+        switch (mode)
+        {
+            case 0:
+                Mode = LoginMode.NONE;
+                break;
+            case 1:
+                Mode = LoginMode.REGISTER;
+                break;
+            case 2:
+                Mode = LoginMode.LOGIN;
+                break;
+        }
     }
     
     /// <summary>
@@ -146,19 +222,37 @@ public class Login : MonoBehaviour
     /// </summary>
     public void ShowPasswordField()
     {
-        switch (PasswordInput.contentType)
+        switch (Mode)
         {
-            case TMP_InputField.ContentType.Standard:
-                Debug.Log("Adius");
-                PasswordInput.contentType = TMP_InputField.ContentType.Password;
+            case LoginMode.REGISTER:
+                switch (R_PasswordInput.contentType)
+                {
+                    case TMP_InputField.ContentType.Standard:
+                        R_PasswordInput.contentType = TMP_InputField.ContentType.Password;
+                        break;
+                    case TMP_InputField.ContentType.Password:
+                        R_PasswordInput.contentType = TMP_InputField.ContentType.Standard;
+                        break;
+                }
+        
+                R_PasswordInput.ForceLabelUpdate();
+                
                 break;
-            case TMP_InputField.ContentType.Password:
-                Debug.Log("Hula");
-                PasswordInput.contentType = TMP_InputField.ContentType.Standard;
+            case LoginMode.LOGIN:
+                switch (L_PasswordInput.contentType)
+                {
+                    case TMP_InputField.ContentType.Standard:
+                        L_PasswordInput.contentType = TMP_InputField.ContentType.Password;
+                        break;
+                    case TMP_InputField.ContentType.Password:
+                        L_PasswordInput.contentType = TMP_InputField.ContentType.Standard;
+                        break;
+                }
+        
+                L_PasswordInput.ForceLabelUpdate();
+                
                 break;
         }
-        
-        PasswordInput.ForceLabelUpdate();
     }
 
     #endregion
