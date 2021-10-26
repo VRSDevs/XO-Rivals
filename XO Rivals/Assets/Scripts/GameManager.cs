3dce05cfb1d856efed20139fed942bf8fcef2965
Antonio Ruano <blinx2500@gmail.com>
2021-10-26 11:25:00 +0200

    Cambio de UnityCB

--- XO Rivals/Assets/Scripts/GameManager.cs
+++ XO Rivals/Assets/Scripts/GameManager.cs
@@ -37,7 +37,7 @@ public class GameManager : MonoBehaviour
 
     #region UnityCB
     
-    private void Awake()
+    private void Start()
     {
         _networkController = gameObject.AddComponent<NetworkController>();
         DontDestroyOnLoad(this);
