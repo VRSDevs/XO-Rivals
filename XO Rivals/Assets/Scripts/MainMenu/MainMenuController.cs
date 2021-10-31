a30448e94e7790dc665ee2797fd8215fd64fff7b
Antonio Ruano <blinx2500@gmail.com>
2021-10-31 21:45:31 +0100

    Implementación de algoritmo

--- XO Rivals/Assets/Scripts/MainMenu/MainMenuController.cs
+++ XO Rivals/Assets/Scripts/MainMenu/MainMenuController.cs
@@ -1,11 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] public GameObject MainMenuObject;
+    [SerializeField] public GameObject PlayMenuObject;
+    
+    [SerializeField] public Button CreateGameButton;
+    [SerializeField] public Button JoinGameButton;
+    [SerializeField] public TMP_Text JoinOrBackButton_Text;
+    
     public GameManager _gameManager;
     public PlayerInfo _localPlayer;
 
@@ -25,5 +35,23 @@ public class MainMenuController : MonoBehaviour
     {
         _gameManager._networkController.OnConnectToRandomRoom();
         _gameManager.OnConnectToRoom(_localPlayer);
+    }
+
+    public void OnBackButtonClick()
+    {
+        if (PhotonNetwork.CurrentRoom != null)
+        {
+            JoinOrBackButton_Text.text = "Back";
+
+            CreateGameButton.interactable = true;
+            JoinGameButton.interactable = true;
+            
+            _gameManager._networkController.OnLeaveRoom();
+        }
+        else
+        {
+            MainMenuObject.SetActive(true);
+            PlayMenuObject.SetActive(false);
+        }
     }
 }
