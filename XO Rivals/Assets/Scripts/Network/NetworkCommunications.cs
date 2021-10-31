71663477f0ebc5453b54e44cf82e184599f1bb3d
Antonio Ruano <blinx2500@gmail.com>
2021-10-31 02:41:21 +0200

    Generación clase para las comunicaciones

---
+++ XO Rivals/Assets/Scripts/Network/NetworkCommunications.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkCommunications : MonoBehaviourPun
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public void SendPackage()
+    {
+        Package packageToSend = FindObjectOfType<ButtonsScript>().gameState.ToPackage(FindObjectOfType<ButtonsScript>().localPlayer);
+        photonView.RPC("RPC_UpdateTurn", RpcTarget.All, packageToSend);
+    }
+    
+    #region RPCMethods
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="recievedPackage"></param>
+    [PunRPC]
+    public void RPC_UpdateTurn(Package recievedPackage)
+    {
+        FindObjectOfType<GameManager>().MatchId = recievedPackage.MatchId;
+        FindObjectOfType<GameManager>().OwnerId = recievedPackage.OwnerId;
+        if (FindObjectOfType<PlayerInfo>().Name == FindObjectOfType<GameManager>().PlayerInfoO.Name)
+        {
+            FindObjectOfType<GameManager>().PlayerInfoO = recievedPackage.Player;
+        }
+        else
+        {
+            FindObjectOfType<GameManager>().PlayerInfoX = recievedPackage.Player;
+        }
+        FindObjectOfType<GameManager>().WhosTurn = recievedPackage.WhosTurn;
+        FindObjectOfType<GameManager>().NumFilled = recievedPackage.NumFilled;
+        FindObjectOfType<GameManager>().FilledPositions = recievedPackage.FilledPositions;
+        FindObjectOfType<GameManager>().Chips = recievedPackage.Chips;
+        FindObjectOfType<GameManager>().MiniGameChosen = recievedPackage.MinigameChosen;
+
+        FindObjectOfType<GameManager>().turnMoment = 0;
+
+        FindObjectOfType<ButtonsScript>().UpdateTurn();
+    }
+
+    #endregion
+}
