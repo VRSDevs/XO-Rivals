49267a23f4ce7b1d30e8f9e376882a36df3da6b7
FelipeBermejo <felipe.bermo@gmail.com>
2021-10-31 17:15:10 +0100

    Implementación de Sonidos 1
    Introducción de los sonidos al motor de Unity

---
+++ XO Rivals/Assets/Scripts/MainMenu/SFXManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    private AudioSource managerBoton;
+
+    public AudioClip sonidoBoton1;
+    public AudioClip sonidoBoton2;
+    public AudioClip sonidoBoton3;
+    public AudioClip sonidoBoton4; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        managerBoton = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void playSound1()
+    {
+        managerBoton.PlayOneShot(sonidoBoton1);
+    }
+
+    public void playSound2()
+    {
+        managerBoton.PlayOneShot(sonidoBoton2);
+    }
+
+    public void playSound3()
+    {
+        managerBoton.PlayOneShot(sonidoBoton3);
+    }
+
+    public void playSound4()
+    {
+        managerBoton.PlayOneShot(sonidoBoton4);
+    }
+}
--- XO Rivals/Assets/Scripts/Pistolero/ControlBoton.cs
+++ XO Rivals/Assets/Scripts/Pistolero/ControlBoton.cs
@@ -6,26 +6,32 @@ using UnityEngine.EventSystems;
 public class ControlBoton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
 
-
+    private AudioSource sonidoPistolero;
     public Cronometro controlCrono;
     public GameObject botonExit;
+
+    public AudioClip sonidoRecarga;
+    public AudioClip sonidoDisparo; 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        sonidoPistolero = GetComponent<AudioSource>();
     }
     public void OnPointerDown(PointerEventData eventData)
     {
+        sonidoPistolero.PlayOneShot(sonidoRecarga, 1.0f);
         controlCrono.activarCrono();
+       
     }
 
     public void OnPointerUp(PointerEventData eventData)
-    {
+    {      
         controlCrono.finCrono();
+        sonidoPistolero.PlayOneShot(sonidoDisparo, 1.0f);
         this.gameObject.SetActive(false);
         botonExit.SetActive(true);
-      
+       
 
     }
 
