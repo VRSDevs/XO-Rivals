bf4e2ee5230e56a690ac2ea4e018cd56586e603b
Requena21 <alejandroorequena21@gmail.com>
2021-11-04 20:34:39 +0100

    Fallo con la instalación de dotween

--- XO Rivals/Assets/Scripts/PlatformMinigame/PlatformMinigameController.cs
+++ XO Rivals/Assets/Scripts/PlatformMinigame/PlatformMinigameController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using DG.Tweening;
 
 public class PlatformMinigameController : MonoBehaviour
 { 
@@ -8,6 +9,9 @@ public class PlatformMinigameController : MonoBehaviour
     // Gamemanager
     private GameManager _gameManager;
 
+    // Camera for movement
+    [SerializeField]
+    private Camera cam;
 
     // Start is called before the first frame update
     void Start()
