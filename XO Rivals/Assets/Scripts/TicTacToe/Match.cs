c5b825e42fc2cd7908d93ba00c1c319dfff7768b
Antonio Ruano <blinx2500@gmail.com>
2021-11-10 19:09:37 +0100

    Variables privadas
    Definición de las variables privadas de la partida

--- XO Rivals/Assets/Scripts/TicTacToe/Match.cs
+++ XO Rivals/Assets/Scripts/TicTacToe/Match.cs
@@ -2,17 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Match : MonoBehaviour
+public class Match
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    #region Vars
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    /// <summary>
+    /// ID de la partida
+    /// </summary>
+    private string _matchId;
+    /// <summary>
+    /// ID del dueño de la partida
+    /// </summary>
+    private string _ownerId;
+    /// <summary>
+    /// Nombre del jugador O
+    /// </summary>
+    private string _playerO;
+    /// <summary>
+    /// Nombre del jugador X
+    /// </summary>
+    private string _playerX;
+    /// <summary>
+    /// Nombre del jugador actual
+    /// </summary>
+    private string _whosTurn;
+    /// <summary>
+    /// Momento de la partida en la que se encuentra el jugador
+    /// </summary>
+    private int _turnMoment;
+    /// <summary>
+    /// Número de fichas colocadas en el tablero
+    /// </summary>
+    private int _filledPositions;
+    /// <summary>
+    /// Array del tablero
+    /// </summary>
+    private int _numFilled;
+    /// <summary>
+    /// Lista de las fichas
+    /// </summary>
+    private List<GameObject> _chips;
+
+    #endregion
+
+    #region Getters&Setters
+
+    
+    
+
+    #endregion
+    
+    
 }
