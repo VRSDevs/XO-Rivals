00f2388c0f0dad5491fe07e5dc4ffb945ad9669d
Requena21 <alejandroorequena21@gmail.com>
2021-11-11 17:03:03 +0100

    Script de movimiento del personaje del minijuego fantasma

--- XO Rivals/Assets/Scripts/Fantasmas/MovePlayer.cs
+++ XO Rivals/Assets/Scripts/Fantasmas/MovePlayer.cs
@@ -6,29 +6,19 @@ using UnityEngine.InputSystem;
 public class MovePlayer : MonoBehaviour
 {
 
-    //POSICION NEGRO = x:Plyaer +0.7    y:pñlayer -0.87
+    //POSICION NEGRO = x:Plyaer +0.7    y:pï¿½layer -0.87
     [SerializeField] private float speed;
-    private PlayerActionsController playerActionsController;
-    private float movementInputx;
-    private float movementInputy;
-    private Rigidbody2D body;
-
-    // Controles de movil
-    [SerializeField]
-    public GameObject leftButton;
-    [SerializeField]
-    public GameObject rightButton;
-    [SerializeField]
-    public GameObject topButton;
-    [SerializeField]
-    public GameObject botButton;
+    private float horizontal;
+    private float vertical;
+    private Rigidbody2D player;
 
     public GameObject sombra;
 
+    private bool vivo = true;
+
     private void Awake()
     {
-        playerActionsController = new PlayerActionsController();
-        body = GetComponent<Rigidbody2D>();
+        player = GetComponent<Rigidbody2D>();
     }
 
     // Start is called before the first frame update
@@ -40,57 +30,32 @@ public class MovePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       // sombra.transform.position = new Vector3(this.transform.position.x+0.9f, this.transform.position.y-0.87f, this.transform.position.z);
+        sombra.transform.position = new Vector3(this.transform.position.x+0.9f, this.transform.position.y-0.87f, this.transform.position.z);
+
+        if (vivo)
+        {
+            player.velocity = new Vector2(horizontal * speed, vertical * speed);
+        }
     }
 
     private void FixedUpdate()
-    {
-        body.velocity = new Vector2(movementInputx * speed, movementInputy*speed);
+    {   
 
     }
 
-    public void OnEnable()
+
+    public void MoveHorizontal(InputAction.CallbackContext context)
     {
-        playerActionsController.Enable();
+        if (context.ReadValue<Vector2>().x < 0){ return; }
+
+        horizontal = context.ReadValue<Vector2>().x;
     }
 
-    public void OnDisable()
+    public void MoveVertical(InputAction.CallbackContext context)
     {
-        playerActionsController.Disable();
+        if (context.ReadValue<Vector2>().y < 0) { return; }
+
+        vertical = context.ReadValue<Vector2>().y;
     }
-
-
-
-    //public void MoveLeft()
-    //{
-    //    movementInputx = -1;
-    //}
-
-    //public void MoveRight()
-    //{
-    //    movementInputx = 1;
-    //}
-
-    //public void MoveTop()
-    //{
-    //    movementInputy = 1;
-    //}
-
-    //public void MoveBot()
-    //{
-    //    movementInputy = -1;
-    //}
-
-    //public void Stop()
-    //{
-    //    movementInputx = 0;
-    //    movementInputy = 0;
-    //}
-
-    //public void Move(InputAction.CallbackContext context)
-    //{
-    //    movementInputx = context.ReadValue<Vector2>().x;
-    //    movementInputy = context.ReadValue<Vector2>().y;
-    //}
 
 }
