99de093abc40e3414fbfccdf1f2747c35bc0910d
Requena21 <alejandroorequena21@gmail.com>
2021-11-17 22:47:37 +0100

    Splash screen video

--- XO Rivals/Assets/Scripts/SplashVideo/SplashVideoManager.cs
+++ XO Rivals/Assets/Scripts/SplashVideo/SplashVideoManager.cs
@@ -5,14 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class SplashVideoManager : MonoBehaviour
 {
+
+    public Animator transition;
+
+    public float transitionTime = 3;
+         
     void Start()
     {
         Invoke("GoToNextScene", 4.5f);
     }
 
-
     void GoToNextScene()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
     }
 }
