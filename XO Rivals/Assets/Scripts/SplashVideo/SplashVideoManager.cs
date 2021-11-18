fad90351d8b6997e610d86616245ccf8deb1e7bf
Requena21 <alejandroorequena21@gmail.com>
2021-11-18 17:31:12 +0100

    video visible al pulsar la pantalla

--- XO Rivals/Assets/Scripts/SplashVideo/SplashVideoManager.cs
+++ XO Rivals/Assets/Scripts/SplashVideo/SplashVideoManager.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class SplashVideoManager : MonoBehaviour
 {
-         
+
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+
+    private void Awake()
+    {
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "intro.mp4");
+    }
+
     void Start()
     {
+        videoPlayer.Play();
         Invoke("GoToNextScene", 4.5f);
     }
 
