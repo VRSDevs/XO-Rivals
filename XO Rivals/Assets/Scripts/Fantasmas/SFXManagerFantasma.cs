e82afdc92be4f0a1dfcf04d06946b2d99370ff56
Nachete07 <ignaciocallesicq@gmail.com>
2021-11-20 12:49:56 +0100

    Sonidos del fantasmas

--- XO Rivals/Assets/Scripts/Fantasmas/SFXManagerFantasma.cs
+++ XO Rivals/Assets/Scripts/Fantasmas/SFXManagerFantasma.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class SFXManagerFantasma : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private AudioSource managerFantasmas;
+    public AudioClip CookingSound;
+    public AudioClip DeathSound;
+    public AudioClip VictorySound;
+    public AudioClip DefeatSound;
+    public AudioClip WalkSound;
+    
+    
     void Start()
     {
-        
+        managerFantasmas = GetComponent<AudioSource>();
+    }
+    public void playDeathSound()
+    {
+        managerFantasmas.PlayOneShot(DeathSound);
+    }
+    public void playVictorySound()
+    {
+        managerFantasmas.PlayOneShot(VictorySound);
+    }
+    public void playDefeatSound()
+    {
+        managerFantasmas.PlayOneShot(DefeatSound);
+    }
+    public void playWalkSound()
+    {
+        managerFantasmas.PlayOneShot(WalkSound);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
