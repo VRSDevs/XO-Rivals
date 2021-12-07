9a5cb9630d14d9ef9fc697364fa1f0122cf57ad6
Antonio Ruano <blinx2500@gmail.com>
2021-12-07 11:26:17 +0100

    Método para recibir datos
    Estructura del método

--- XO Rivals/Assets/Scripts/Network/CloudDataController.cs
+++ XO Rivals/Assets/Scripts/Network/CloudDataController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using PlayFab.ClientModels;
 using UnityEngine;
 
 [Serializable]
@@ -12,5 +13,40 @@ public enum DataType
 
 public class CloudDataController
 {
-    
+
+    #region ClouldMethods
+
+    public Dictionary<string, string> GetDataFromCloud(GetUserDataResult result, DataType type)
+    {
+        if (result == null) return null;
+        
+        Debug.Log("Se han recibido datos");
+
+        try
+        {
+            switch (type)
+            {
+                case DataType.Lives:
+
+                    if (!result.Data.ContainsKey("Lives")) throw new Exception();
+
+                    return new Dictionary<string, string>()
+                    {
+                        {"Lives", result.Data["Lives"].Value}
+                    };
+                
+                case DataType.Level:
+
+                    break;
+            }
+        }
+        catch (Exception e)
+        {
+            // ignored
+        }
+
+        return null;
+    }
+
+    #endregion
 }
