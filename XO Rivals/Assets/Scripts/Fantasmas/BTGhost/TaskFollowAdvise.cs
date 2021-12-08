7ac98d5b1f1b9aab9d90090114ce4a736d174071
kalomano <sowerpablo@gmail.com>
2021-12-08 23:05:26 +0100

    TaskFollowAdvise creado
    Este Task del arbol de comportamiento de los fantasmas sigue la posicion de el aviso que le han dado

---
+++ XO Rivals/Assets/Scripts/Fantasmas/BTGhost/TaskFollowAdvise.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorTree;
+
+public class TaskFollowAdvise : Node
+{
+
+
+    private EnemyBT _tree;
+    private EnemyNavMesh _enemyNav;
+
+
+    public TaskFollowAdvise(EnemyBT tree, EnemyNavMesh enemyNav)
+    {
+        _tree = tree;
+        _enemyNav = enemyNav;
+
+    }
+
+
+
+
+    public override NodeState Evaluate()
+    {
+
+
+        if (Vector3.Distance(_enemyNav.transform.position, _tree.adviseTransform.position) < 0.1f) //HASTA QUE NO LLEGUE A LA POSICION INDICADA NO PARA
+        {
+            _tree.setAdviseFalse();
+
+        }
+
+        state = NodeState.RUNNING;
+        return state;
+
+    }
+
+
+}
