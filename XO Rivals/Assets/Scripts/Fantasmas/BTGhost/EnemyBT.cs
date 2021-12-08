486b37478018f9e862d685b7a88a10956dd17fde
kalomano <sowerpablo@gmail.com>
2021-12-08 22:34:12 +0100

    Método wait en BT
    añadimos un método que permite ajuistar espera en el árbol de comportamiento

--- XO Rivals/Assets/Scripts/Fantasmas/BTGhost/EnemyBT.cs
+++ XO Rivals/Assets/Scripts/Fantasmas/BTGhost/EnemyBT.cs
@@ -6,14 +6,36 @@ using BehaviorTree;
 public class EnemyBT : BehaviorTree.Tree
 {
     public EnemyNavMesh enemyNav;
-
+    public DetectPlayer detectPlayer;
 
     protected override Node SetupTree()
     {
 
 
-        Node root = new TaskPatrol(enemyNav);
+        Node root = new Selector(new List<Node>
+        {
+            //new Sequence(new List<Node>
+            //{
+            //    new CheckEnemyInAttackRange(transform),
+            //    new TaskAttack(transform),
+            //}),
+            new Sequence(new List<Node>
+            {
+                new CheckLinterna(detectPlayer),
+                new TaskFollowPlayer(enemyNav),
+            }),
+            new TaskPatrol(enemyNav),
+      
+
+        });
+
         return root;
 
     }
+
+    public void wait(int seconds)
+    {
+        waitTime = 1;
+    }
+
 }
