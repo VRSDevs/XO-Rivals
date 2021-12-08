ca61fce1463a68842ba64d8fa126c7feb71d8697
kalomano <sowerpablo@gmail.com>
2021-12-08 19:35:16 +0100

    TaskFollowPlayer creado
    Creamos el task del arbol que persigue al jugador directamente.

---
+++ XO Rivals/Assets/Scripts/Fantasmas/BTGhost/TaskFollowPlayer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorTree;
+
+public class TaskFollowPlayer : Node
+{
+
+    private Transform taskTarget;
+    private EnemyNavMesh _enemyNav;
+
+
+    public TaskFollowPlayer(EnemyNavMesh enemyNav)
+    {
+        _enemyNav = enemyNav;
+
+    }
+
+
+
+
+    public override NodeState Evaluate()
+    {
+     
+
+        _enemyNav.target = _enemyNav.player;
+
+
+
+        state = NodeState.RUNNING;
+        return state;
+
+    }
+
+
+
+}
