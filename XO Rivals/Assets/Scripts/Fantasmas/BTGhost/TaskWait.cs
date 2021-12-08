d05df3bb037b7234c07ff607b32af4f990e23dea
kalomano <sowerpablo@gmail.com>
2021-12-08 21:14:52 +0100

    TaskWait creado
    Creamos el task del arbol que espera x segundos.

---
+++ XO Rivals/Assets/Scripts/Fantasmas/BTGhost/TaskWait.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorTree;
+
+public class TaskWait : Node
+{
+
+    private EnemyBT _tree;
+    private float time = 0;
+    private int _waitTime;
+
+
+    public TaskWait(EnemyBT tree, int waitTime)
+    {
+        _tree = tree;
+        _waitTime = waitTime;
+
+    }
+
+
+
+
+    public override NodeState Evaluate()
+    {
+        Debug.Log("EXECUTE TASKWAIT");
+
+        _tree.wait(_waitTime);
+
+        state = NodeState.SUCCESS;
+        return state;
+
+
+    }
+
+}
