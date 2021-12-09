1f771cd31772326303acd8fba3c798f7ce78e22f
kalomano <sowerpablo@gmail.com>
2021-12-09 19:25:29 +0100

    destruir huella

--- XO Rivals/Assets/Scripts/Fantasmas/ScriptHuella.cs
+++ XO Rivals/Assets/Scripts/Fantasmas/ScriptHuella.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class ScriptHuella : MonoBehaviour
 {
+
+    public GameObject sigHuella;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        StartCoroutine(deleteHuella());
+
+
     }
 
     // Update is called once per frame
@@ -15,4 +21,14 @@ public class ScriptHuella : MonoBehaviour
     {
         
     }
+
+
+    IEnumerator deleteHuella()
+    {
+        yield return new WaitForSeconds(7f);
+
+        Destroy(this.gameObject);
+
+    }
+
 }
