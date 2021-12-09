ca3d9c6b30688e0693dddc53051a80ca8d0a702d
kalomano <sowerpablo@gmail.com>
2021-12-09 19:10:36 +0100

    Codigo crearHuella
    codigo c# instanciar prefab

--- XO Rivals/Assets/Scripts/Fantasmas/CrearHuellas.cs
+++ XO Rivals/Assets/Scripts/Fantasmas/CrearHuellas.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class CrearHuellas : MonoBehaviour
 {
+
+    public GameObject prefabHuella;
+    public GameObject huella;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(creaHuella());
     }
 
     // Update is called once per frame
@@ -15,4 +19,17 @@ public class CrearHuellas : MonoBehaviour
     {
         
     }
+    IEnumerator creaHuella()
+    {
+        yield return new WaitForSeconds(2f);
+
+        //INSTANCIAR PREFAB
+        huella = Instantiate(prefabHuella, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.identity);
+        huella.GetComponent<BalaDetectScript>().crear(this.gameObject);
+
+
+        //CODE
+        StartCoroutine(creaHuella());
+    }
+
 }
