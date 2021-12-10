464ff1d017352e4db1c5e4804d7d36759d2964e1
Antonio Ruano <blinx2500@gmail.com>
2021-12-10 23:38:23 +0100

    Método GetString
    Generación del método para el enumerador de items para obtener la ID correspondiente

--- XO Rivals/Assets/Scripts/Network/PurchasesController.cs
+++ XO Rivals/Assets/Scripts/Network/PurchasesController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,43 @@ public enum ShopItem
     Lives10,
     Lives30,
     LivesInf
+}
+
+/// <summary>
+/// Clase auxiliar para el enumerador ShopItem
+/// </summary>
+public static class ShopItemExtension
+{
+    /// <summary>
+    /// Método para transformar un valor del enumerador en una cadena de texto
+    /// </summary>
+    /// <param name="item">Item del cual obtener el ID</param>
+    /// <returns>ID del item solicitado</returns>
+    public static string GetString(this ShopItem item)
+    {
+        switch (item)
+        {
+            case ShopItem.Life1:
+                return "onelfpck";
+            case ShopItem.Lives3:
+                return "threelvspck";
+            case ShopItem.Lives5:
+                return "fivelvspck";
+            case ShopItem.Lives10:
+                return "tenlvspck";
+            case ShopItem.Lives30:
+                return "thirtylvspck";
+            case ShopItem.LivesInf:
+                return "inflvspck";
+            default:
+                return "";
+        }
+    }
+
+    public static string GetCatalog(this ShopItem item)
+    {
+        
+    }
 }
 
 public class PurchasesController : MonoBehaviour
