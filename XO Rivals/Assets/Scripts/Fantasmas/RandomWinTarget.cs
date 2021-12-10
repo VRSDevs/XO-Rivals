c092a8fbf71d0edc3ebe13f4664043924b4f95c6
kalomano <sowerpablo@gmail.com>
2021-12-10 17:20:10 +0100

    Creo salida aleatoria
    Start Random

--- XO Rivals/Assets/Scripts/Fantasmas/RandomWinTarget.cs
+++ XO Rivals/Assets/Scripts/Fantasmas/RandomWinTarget.cs
@@ -8,6 +8,51 @@ public class RandomWinTarget : MonoBehaviour
     void Start()
     {
         
+        int randomWin = Random.Range(0,3);
+
+        if (randomWin == 0) //SALIDA 0
+        {
+           
+            GameObject.FindGameObjectsWithTag("Win2")[0].SetActive(false);
+            GameObject.FindGameObjectsWithTag("Win3")[0].SetActive(false);
+            GameObject.FindGameObjectsWithTag("Win4")[0].SetActive(false);
+
+            GameObject.Find("SalidaCerrada1").SetActive(false);
+
+        }
+
+        if (randomWin == 1) //SALIDA 0
+        {
+
+            GameObject.FindGameObjectsWithTag("Win1")[0].SetActive(false);
+            GameObject.FindGameObjectsWithTag("Win3")[0].SetActive(false);
+            GameObject.FindGameObjectsWithTag("Win4")[0].SetActive(false);
+
+            GameObject.Find("SalidaCerrada2").SetActive(false);
+
+        }
+
+        if (randomWin == 2) //SALIDA 0
+        {
+            GameObject.FindGameObjectsWithTag("Win1")[0].SetActive(false);
+            GameObject.FindGameObjectsWithTag("Win2")[0].SetActive(false);
+            GameObject.FindGameObjectsWithTag("Win4")[0].SetActive(false);
+
+            GameObject.Find("SalidaCerrada3").SetActive(false);
+
+        }
+
+        if (randomWin == 3) //SALIDA 0
+        {
+            GameObject.FindGameObjectsWithTag("Win1")[0].SetActive(false);
+            GameObject.FindGameObjectsWithTag("Win2")[0].SetActive(false);
+            GameObject.FindGameObjectsWithTag("Win3")[0].SetActive(false);
+
+            GameObject.Find("SalidaCerrada4").SetActive(false);
+
+        }
+
+
     }
 
     // Update is called once per frame
