3dbeea9d7d924e51b562dccfb55da10cead506a4
kalomano <sowerpablo@gmail.com>
2021-12-10 12:35:16 +0100

    Creamos script detectHuella
    Para detectar si el enemigo encuentra una huella

---
+++ XO Rivals/Assets/Scripts/Fantasmas/DetectHuella.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectHuella : MonoBehaviour
+{
+
+    private EnemyBT enemyBT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyBT = this.GetComponent<EnemyBT>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Huella(Clone)")
+        {
+            enemyBT.lastHuella = collision.gameObject;
+            enemyBT.perseguirHuella = true;
+
+        }
+
+    }
+
+
+}
