66e70e64d7f62236cf6744a95fe01181a12aa98f
AlberVicky09 <albervicky09@gmail.com>
2021-12-10 20:47:58 +0100

    TicTacAI

--- XO Rivals/Assets/Scripts/TicTacToe/TicTacAI.cs
+++ XO Rivals/Assets/Scripts/TicTacToe/TicTacAI.cs
@@ -17,7 +17,7 @@ public class TicTacAI : MonoBehaviour
         
     }
 
-    private int MiniMax(int[,] board, int originalTurn, int turn, int numFilled, int lastCol, int lastRow){
+    /*private int MiniMax(int[,] board, int originalTurn, int turn, int numFilled, int lastCol, int lastRow){
 
         //Check if its victory state
         int victory = CheckVictory(board);
@@ -59,126 +59,301 @@ public class TicTacAI : MonoBehaviour
         }
 
         return score;
-    }
-
+    }*/
     
     #region TestMethods
 
     //Optimize this using last tile
-    int CheckVictory(int[,] board){
+    bool CheckVictory(int[,] board, int player, ref int row, ref int col){
 
         for(int i = 0; i < 3; i++){
-            if(TestCol(i) == true)
-                return board[i,0] == 0 ? -1 : 1;
+            if(TestCol(i, player, ref row) == true)
+                return true;
         }
 
         for(int i = 0; i < 3; i++){
-            if(TestRow(i) == true)
-                return board[i,0] == 0 ? -1 : 1;
+            if(TestRow(i, player, ref col) == true)
+                return true;
         }
 
         for(int i = 0; i < 2; i++){
-            if(TestDiag(i) == true)
-               return board[i,0] == 0 ? -1 : 1;
+            if(TestDiag(i, player, ref row, ref col) == true)
+               return true;
         }
 
-        return 0;
+        return false;
     }
 
-    bool TestCol(int col){
-        int type;
-        int j = 0;
+    bool TestCol(int col, int player, ref int row){
 
-        //Pick first tile in column if its not empty
-        if(board[col,j] != 3){
-            type = board[col,j];
-            j++;
-        }else{
-            return false;
-        }
+        bool found = false;
+        int j = -1;
 
-        //Check if all other tiles are the same
+        //Pick first tile in column that is not empty
         do{
-            if(board[col,j] != type){
+            //If its the last tile, its imposible to win
+            j++;
+            if(j == 2){
                 return false;
             }
-            j++;
-        }while(j < 3);
 
-        return true;
+            //If its not empty
+            if(board[j,col] != 3){
+                //Check if its same as player
+                if(board[j,col] == player){
+                    found = true;
+                }else{
+                    return false;
+                }
+            }
+            
+        }while(!found);
+
+        //Check rest of the tiles
+        j++;
+        switch(j){
+
+            //If found on first, there are two options 
+            case 1:
+                //If mid is empty, third must be player
+                if(board[j,col] == 3){
+                    j++;
+                    if(board[j,col] == player){
+                        row = 1;
+                        return true;
+                    }else{
+                        return false;
+                    }
+                //If mid is player, third must be empty
+                }else if(board[j,col] == player){
+                    j++;
+                    if(board[j,col] == 3){
+                        row = j;
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }else{
+                    return false;
+                }
+
+            //If found on mid, last must match
+            case 2:
+                if(board[j,col] == player){
+                    row = 0;
+                    return true;
+                }else{
+                    return false;
+                }
+        }
+
+        return false;
     }
     
-    bool TestRow(int row){
-        int type;
-        int j = 0;
+    bool TestRow(int row, int player, ref int col){
 
-        //Pick first tile in column if its not empty
-        if(board[j,row] != 3){
-            type = board[j,row];
-            j++;
-        }else{
-            return false;
-        }
+        bool found = false;
+        int j = -1;
 
-        //Check if all other tiles are the same
+        //Pick first tile in column that is not empty
         do{
-            if(board[j,row] != type){
+            //If its the last tile, its imposible to win
+            j++;
+            if(j == 2){
                 return false;
             }
-            j++;
-        }while(j < 3);
 
-        return true;
+            //If its not empty
+            if(board[row,j] != 3){
+                //Check if its same as player
+                if(board[row,j] == player){
+                    found = true;
+                }else{
+                    return false;
+                }
+            }
+            
+        }while(!found);
+
+        //Check rest of the tiles
+        j++;
+        switch(j){
+
+            //If found on first, there are two options 
+            case 1:
+                //If mid is empty, third must be player
+                if(board[row,j] == 3){
+                    j++;
+                    if(board[row,j] == player){
+                        col = 1;
+                        return true;
+                    }else{
+                        return false;
+                    }
+                //If mid is player, third must be empty
+                }else if(board[row,j] == player){
+                    j++;
+                    if(board[row,j] == 3){
+                        col = j;
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }else{
+                    return false;
+                }
+
+            //If found on mid, last must match
+            case 2:
+                if(board[row,j] == player){
+                    col = 0;
+                    return true;
+                }else{
+                    return false;
+                }
+        }
+
+        return false;
     }
 
-    bool TestDiag(int diag){
-        int type;
-        int j = 0;
+    bool TestDiag(int diag, int player, ref int row, ref int col){
+
+        bool found = false;
+        int j = -1;
 
         //First diagonal
         if(diag == 0){
-            //Pick first tile in column if its not empty
-            if(board[diag,j] != 3){
-                type = board[diag,j];
-                j++;
-                diag++;
-            }else{
-                return false;
-            }
 
-            //Check if all other tiles are the same
+            //Pick first tile in diagonal that is not empty
             do{
-                if(board[diag,j] != type){
+                //If its the last tile, its imposible to win
+                j++;
+                if(j == 2){
                     return false;
                 }
-                diag++;
-                j++;
-            }while(j < 3);
 
-            return true;
+                //If its not empty
+                if(board[j,j] != 3){
+                    //Check if its same as player
+                    if(board[j,j] == player){
+                        found = true;
+                    }else{
+                        return false;
+                    }
+                }
+            }while(!found);
+
+            //Check rest of the tiles
+            j++;
+            switch(j){
+
+                //If found on first, there are two options 
+                case 1:
+                    //If mid is empty, third must be player
+                    if(board[j,j] == 3){
+                        j++;
+                        if(board[j,j] == player){
+                            col = row = 1;
+                            return true;
+                        }else{
+                            return false;
+                        }
+                    //If mid is player, third must be empty
+                    }else if(board[j,j] == player){
+                        j++;
+                        if(board[j,j] == 3){
+                            col = row = j;
+                            return true;
+                        }else{
+                            return false;
+                        }
+                    }else{
+                        return false;
+                    }
+
+                //If found on mid, last must match
+                case 2:
+                    if(board[j,j] == player){
+                        col = row = 0;
+                        return true;
+                    }else{
+                        return false;
+                    }
+            }
+
+            return false;
 
         //Second diagonal
         }else{
-            diag++;
-            //Pick first tile in column if its not empty
-            if(board[diag,j] != 3){
-                type = board[diag,j];
-                j++;
-                diag--;
-            }else{
-                return false;
-            }
 
-            //Check if all other tiles are the same
+            j = 3;
+            int k = -1;
+
+            //Pick first tile in column that is not empty
             do{
-                if(board[diag,j] != type){
+                //If its the last tile, its imposible to win
+                j--;
+                k++;
+                if(k == 2){
                     return false;
                 }
-                diag--;
-                j++;
-            }while(j < 3);
 
-            return true;
+                //If its not empty
+                if(board[k,j] != 3){
+                    //Check if its same as player
+                    if(board[k,j] == player){
+                        found = true;
+                    }else{
+                        return false;
+                    }
+                }
+                
+            }while(!found);
+
+            //Check rest of the tiles
+            j--;
+            k++;
+            switch(k){
+
+                //If found on first, there are two options 
+                case 1:
+                    //If mid is empty, third must be player
+                    if(board[k,j] == 3){
+                        j--;
+                        k++;
+                        if(board[k,j] == player){
+                            col = row = 1;
+                            return true;
+                        }else{
+                            return false;
+                        }
+                    //If mid is player, third must be empty
+                    }else if(board[k,j] == player){
+                        j--;
+                        k++;
+                        if(board[k,j] == 3){
+                            row = k;
+                            col = j;
+                            return true;
+                        }else{
+                            return false;
+                        }
+                    }else{
+                        return false;
+                    }
+
+                //If found on mid, last must match
+                case 2:
+                    if(board[k,j] == player){
+                        row = j;
+                        col = k;
+                        return true;
+                    }else{
+                        return false;
+                    }
+            }
+
+            return false;
         }
     }
 
